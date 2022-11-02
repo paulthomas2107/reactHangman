a80e505cab5c6deff39657ca25a81c3033a9261f
Paul Thomas <paulthomas2107@gmail.com>
2022-11-02 13:24:06 +0000

    More

--- hangman/src/App.tsx
+++ hangman/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { HangmanDrawing } from './HangmanDrawing';
 import { HangmanWord } from './HangmanWord';
 import { Keyboard } from './Keyboard';
@@ -9,30 +9,36 @@ function App() {
     return words[Math.floor(Math.random() * words.length)];
   });
 
+  console.log(wordToGuess);
+
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
   const incorrectLetters = guessedLetters.filter(
     (letter) => !wordToGuess.includes(letter)
   );
 
-  function addGuessedLetter(letter: string) {
-    if (guessedLetters.includes(letter)) return
-    
-  }
+  const addGuessedLetter = useCallback(
+    (letter: string) => {
+      if (guessedLetters.includes(letter)) return;
+
+      setGuessedLetters((currentLetters) => [...currentLetters, letter]);
+    },
+    [guessedLetters]
+  );
 
   useEffect(() => {
     const handler = (e: KeyboardEvent) => {
       const key = e.key;
       if (!key.match(/^[a-z]$/)) return;
-      e.preventDefault()
-      addGuessedLetter(key)
+      e.preventDefault();
+      addGuessedLetter(key);
     };
     document.addEventListener('keypress', handler);
 
     return () => {
       document.removeEventListener('keypress', handler);
     };
-  }, []);
+  }, [guessedLetters]);
 
   return (
     <div
@@ -56,7 +62,13 @@ function App() {
       <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
       <HangmanWord guessedLetters={guessedLetters} wordToGuess={wordToGuess} />
       <div style={{ alignSelf: 'stretch' }}>
-        <Keyboard />
+        <Keyboard
+          activeLetters={guessedLetters.filter((letter) =>
+            wordToGuess.includes(letter)
+          )}
+          inactiveLetters={incorrectLetters}
+          addGuessedLetter={addGuessedLetter}
+        />
       </div>
     </div>
   );
